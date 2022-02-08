c057b6fdbb35229701d83b9f10302663b5b1fe58
riyantori <riyantori24@gmail.com>
2022-02-09 00:10:42 +0700

    Menambahkan innerTEXT, innerHTML dan style.property pada DOM

--- JavaScript-DOM-Dicoding/dom.js
+++ JavaScript-DOM-Dicoding/dom.js
@@ -9,3 +9,24 @@ console.log(playButton)
 const playButtonElement = playButton.childNodes[0];
 console.log(playButtonElement)
 playButtonElement.setAttribute("disabled", "button dimatikan");
+
+const links = document.getElementById("links");
+console.log(links)
+console.log(links.innerHTML)
+console.log(links.innerText)
+
+const dicoding = document.getElementById("dicodingLink");
+console.log(dicoding)
+dicoding.innerText = "Belajar Programming di Dicoding";
+dicoding.innerHTML = "<i>Belajar Programming di Dicoding</i>"
+
+const google = document.getElementById("googleLink");
+google.innerText = "Belajar Programming di Google";
+google.innerHTML = "<i>Belajar Programming di Google</i>"
+
+const buttonBaru = document.getElementsByClassName("button");
+
+for (button of buttonBaru) {
+    console.log(button.childNodes[0])
+    button.childNodes[0].style.borderRadius = "10px";
+}
