9904cebf8786c97188986801f166ed2fa9b00c81
riyantori <riyantori24@gmail.com>
2022-02-11 21:56:43 +0700

    Membuat custom-event

---
+++ JavaScript-DOM-Dicoding/custom-event.js
@@ -0,0 +1,14 @@
+const changeCaption = new Event("changeCaption")
+
+window.addEventListener('load', function(){
+    const tombol = document.getElementById("tombol");
+    tombol.addEventListener("changeCaption", customEventHendler);
+    tombol.addEventListener("click", function(){
+        tombol.dispatchEvent(changeCaption);
+    });
+});
+function customEventHendler(ev){
+    console.log("Event " + ev.type + "telah dijalankan");
+    const caption = document.getElementById("caption");
+    caption.innerText = "Anda telah membangkitkan custom event";
+}
